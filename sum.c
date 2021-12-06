af96498b9b6ee2fc708cba848a5c326d765aaaea
Parth-Joshi03 <parthjoshi0307@gmail.com>
2021-12-06 09:47:02 +0530

    Sum of Two numbers

---
+++ sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+	int a,b,sum,mult,sub;
+	a=9;
+	b=16;
+	sum=a+b;
+	mult=a*b;
+	sub=a-b;
+   printf("The sum of a and b is %d \n ",sum);
+   printf("Product of a and b is %d \n",mult);
+	printf("Diffrence of a and b is %d",sub);
+	
+}
